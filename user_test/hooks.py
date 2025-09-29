98aa69395a2e2d0db6b411b6d8fffa7bca5cd3a9
Mathieu Benoit <mathben@technolibre.ca>
2025-09-28 23:45:14 -0400

    [FIX] user_test: delete user test before create it, fix chatter_position

--- user_test/hooks.py
+++ user_test/hooks.py
@@ -10,9 +10,10 @@ login = "test"
 
 
 def uninstall_hook(env):
-    user_test = env["res.users"].search([("login", "=", login)])
-    if user_test:
-        user_test.unlink()
+    _logger.warning("User test is installed, don't forget to uninstall it.")
+    # user_test = env["res.users"].search([("login", "=", login)])
+    # if user_test:
+    #     user_test.unlink()
 
 
 def post_init_hook(env):
@@ -27,10 +28,18 @@ def post_init_hook(env):
     # Copy the profile of default user and copy the system user permission
     system_user = env["res.users"].browse(1)
     first_user = env["res.users"].browse(2)
+    test_user = env["res.users"].search(
+        [("login", "=", login), ("active", "in", [True, False])]
+    )
+    if test_user:
+        test_user.unlink()
     user_test_info = {
-        "name": "test",
+        "name": login,
         "login": login,
-        "new_password": "test",
-        'groups_id': [a.id for a in system_user.groups_id]
+        "new_password": login,
+        "password": login,
+        "groups_id": [a.id for a in system_user.groups_id],
+        "chatter_position": "side",
     }
-    first_user.copy(user_test_info)
+    copied_user = first_user.copy(user_test_info)
+    copied_user.write({"active": True, "new_password": login, "password": login})
